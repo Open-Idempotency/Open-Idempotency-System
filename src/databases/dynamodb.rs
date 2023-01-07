91dc714b3852007280ab40ebb5a006368b3e41ca
Jason Gallavin <jasongallavin@gmail.com>
2023-01-07 17:41:15 -0500

    Get redis test passing

--- src/databases/dynamodb.rs
+++ src/databases/dynamodb.rs
@@ -8,12 +8,13 @@ use crate::databases::database::{DbConfig, IDatabase};
 pub struct DynamodbClient {
     client: Client,
     table_name: String,
+    config: DbConfig
 }
 
 #[async_trait]
 impl IDatabase for DynamodbClient {
 
-    async fn exists(&self, key: String, app_id: String) -> bool {
+    async fn exists(&mut self, key: String, app_id: String) -> bool {
         // let request = &self.client
         //     .get_item()
         //     .table_name(&self.table_name)
@@ -26,7 +27,7 @@ impl IDatabase for DynamodbClient {
         return true
     }
 
-    async fn delete (&self, key: String, app_id: String){
+    async fn delete (&mut self, key: String, app_id: String){
         // let request = client
         //     .delete_item()
         //     .table_name(tablename)
@@ -38,7 +39,7 @@ impl IDatabase for DynamodbClient {
         //     ).send().await?;
     }
 
-    async fn put (&self, key: String, app_id: String, ttl: Option<Duration>){
+    async fn put (&mut self, key: String, app_id: String, ttl: Option<Duration>){
         // let request = client
         //     .put_item()
         //     .table_name(tablename)
@@ -52,17 +53,15 @@ impl IDatabase for DynamodbClient {
         //         AttributeValue::N(ttl.seconds),
         //     ).send().await?;
     }
-
-
-
 }
 
 impl DynamodbClient {
-    pub async fn new (config: DbConfig) -> Arc<dyn IDatabase> {
+    pub async fn new (config: DbConfig) -> Box<dyn IDatabase> {
         let shared_config = aws_config::load_from_env().await;
-        return Arc::new(DynamodbClient {
+        return Box::new(DynamodbClient {
             client: Client::new(&shared_config),
-            table_name: config.table_name.unwrap()
+            table_name: config.table_name.clone().unwrap(),
+            config: config.clone()
         });
     }
 }
--- src/databases/redis_driver.rs
+++ src/databases/redis_driver.rs
@@ -3,54 +3,79 @@ use async_trait::async_trait;
 use std::sync::Arc;
 use std::time::Duration;
 use redis::{Client, Commands, Connection, RedisResult};
-use crate::databases::database::{IDatabase, DbConfig};
+use crate::databases::database::{IDatabase, DbConfig, combine_key};
+use std::convert::TryFrom;
 
 pub struct RedisClient{
-    client: Client
+    client: Client,
+    con: Connection,
+    config: DbConfig
+}
+
+
+fn fetch_an_integer() -> redis::RedisResult<isize> {
+    // connect to redis
+    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let mut con = client.get_connection()?;
+    // throw away the result, just make sure it does not fail
+    let _ : () = con.set("my_key", 42).unwrap();
+    // read back the key and return it.  Because the return value
+    // from the function is a result for integer this will automatically
+    // convert into one.
+    con.get("my_key")
 }
 
 #[async_trait]
 impl IDatabase for RedisClient {
 
-    async fn exists(&self, key: String, app_id: String) -> bool{
-        // let conn = &mut (self.conn).unwrap();
-        // conn.get(format!("{}:{}",self.project_name, key))?;
-        return true;
+    async fn exists(&mut self, key: String, app_id: String) -> bool{
+        let val: bool = self.con.exists(combine_key(key, app_id), ).unwrap();
+        return val;
     }
 
-    async fn delete (&self, key: String, app_id: String){
+    async fn delete (&mut self, key: String, app_id: String){
         // let conn = &mut (self.conn).unwrap();
         // let ret: () = conn.del(key)?;
     }
 
-    async fn put (&self, key: String, app_id: String, ttl: Option<Duration>) {
-        // let conn = &mut (self.conn).unwrap();
-        // let ret : () =  conn.set_ex(format!("{}:{}",self.project_name, key),"",ttl.as_secs().to_usize())?;
+    async fn put (&mut self, key: String, app_id: String, ttl: Option<Duration>) {
+        let ttl_usize = usize::try_from(ttl.unwrap().as_secs()).unwrap();
+        let _ : () = self.con.set_ex(combine_key(key, app_id), 42, ttl_usize).unwrap();
     }
 
 }
 
 impl RedisClient {
-    pub(crate) fn new (config: DbConfig) -> Arc<dyn IDatabase> {
+    pub(crate) fn new (config: DbConfig) -> Box<dyn IDatabase> {
+        let client = Client::open(config.url.clone()).unwrap();
+        let con =  client.get_connection().unwrap();
         let r = RedisClient {
-            client: Client::open(config.url).unwrap()
+            client,
+            con,
+            config: config.clone()
         };
-        return Arc::new(r);
+        return Box::new(r);
     }
+
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    pub fn test_connect() {
-        let c = RedisClient::new(DbConfig{
+    #[tokio::test]
+    pub async fn test_connect() {
+        let mut c = RedisClient::new(DbConfig{
             table_name: None,
             url: String::from("redis://default:redispw@localhost:49153"),
             keyspace: None,
             ttl: None,
         });
-        // c.put("mykey", String::from("my_app"), Some(Duration::from_secs(30)));
+        c.put(String::from("mykey"), String::from("my_app"), Some(Duration::from_secs(30))).await;
+
+        let result = c.exists(String::from("mykey"), String::from("my_app")).await;
+        println!("{:?}", result);
+        assert_eq!(result, true);
     }
+
 }

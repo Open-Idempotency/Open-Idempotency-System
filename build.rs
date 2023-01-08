8900d411c8d3188268689b15df72341b00f89da9
Jason Gallavin <jasongallavin@gmail.com>
2023-01-08 15:45:32 -0500

    Fix build file

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let main_proto_file = env::var("MAIN_PROTO_FILE").unwrap_or("protos/idempotency.proto".into_string());
+    let main_proto_file = env::var("MAIN_PROTO_FILE").unwrap_or(String::from("protos/idempotency.proto"));
     // The environment variable is specified by build process. if you call this
     // code directly, the out dir won't be specified and will fai.
     let out_dir = env::var("OUT_DIR").expect("ENV OUT_DIR NOT FOUND");
@@ -12,8 +12,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_server(true)
         .file_descriptor_set_path(&descriptor_path)
         //.out_dir("./protos/complied")
-        .compile(&[main_proto_file], &[env::current_dir()?])
+        .compile(&[&main_proto_file], &[env::current_dir()?])
         .unwrap_or_else(|e| panic!("protobuf compile error: {}", e));
-    println!("cargo:rerun-if-changed={}", main_proto_file);
+    println!("cargo:rerun-if-changed={}", &main_proto_file);
     Ok(())
 }

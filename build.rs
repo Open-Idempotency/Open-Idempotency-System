1070284692c1cf475674f1606aae1b57601c7264
Zac <43915749+Cliftonz@users.noreply.github.com>
2023-01-08 13:36:27 -0500

    add main proto file override

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let main_proto_file = "protos/idempotency.proto";
+    let main_proto_file = env::var("MAIN_PROTO_FILE").unwrap_or("protos/idempotency.proto".into_string());
     let descriptor_path =
         PathBuf::from(env::var("OUT_DIR").unwrap()).join("idempotency_descriptor.bin");
     tonic_build::configure()
